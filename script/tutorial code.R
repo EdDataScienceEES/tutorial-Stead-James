ec31b775f47fdcd9f1d62e294ebb80d750b5a235
Stead-James <james1wde@gmail.com>
2024-11-21 18:04:19 +0000

    hhh

--- script/tutorial code.R
+++ script/tutorial code.R
@@ -1,23 +1,46 @@
-##advanced data wrangling - focusing on joining datsets with dplyr functions, following this up with pipes and then advanced data wrangling with tidyr
+##advanced data wrangling - focusing on joining datsets with dplyr functions, and then advanced data wrangling with tidyr
 ###our data set is from the national plant survey - 
-##we are going to focus on two datsets occurences and sampleinfowithlatlong - these have corresponding ids 
+##we are going to focus on two datasets occurences and sampleinfowithlatlong - these have corresponding ids 
 
-## the aim is to complete data wrangling such that I would then to be able to produce a spatial map of certain
-## in this tutorial i am going to first show how to merge these datasets by these ids using a family of dplyry functions (umbrella join)
+## aims: to understand intermediate  dyplyr functions + an introduction to tidyr functions
+# understand new dplyr functions and the uses of tidyr
+# learn how to join datasets, and then how to arrange and slice and then recombine datasets 
+# functional uses of all of this
 ## I am expecting prior knowledge of pipes in dplyr + functions like mutate and filter
-## I am then going to switch to the tidry package and look at separating columns and how to replace na values
+## in this tutorial I am going to show how to merge data sets by these ids using a family of dplyry functions (add columns of one to another)
+# then demonstrate the use of arrange function and family of slice functions and touch briefly on bind_rows which can combine datasets (or in our case recombine)
+## I am then going to switch to the tidyr package and look at separating columns and then how to replace NA values
 
 
 library(dplyr)
-occurences <- occurrences_2015to2023
-spatial_data <- sampleinfowithlatlong_2015to2023
+occurences <- read.csv("data/occurrences_2015to2023.csv")
+spatial_data <- read.csv("data/sampleinfowithlatlong_2015to2023.csv")
 
 
 full_data <- full_join(occurences, spatial_data,
                          by = c("sample_id" = "id"))
 
+
+
+length(unique(inner_data$id))
+
+length(unique(occurences$sample_id))
 ##this joins all our data together by sample_id for occurrences and id for spatial_data, if theres any that dont have a match in either data sheet they will still be included 
 ##and the columns from the other data will return NA
+
+library(tibble)
+data <- tibble(
+  id = rep(10, 10),
+  random_numbers = runif(10)
+)
+
+space <- tibble(
+  id = 10,
+  no = 1
+)
+
+full_ex <- full_join(data, space,
+                     by = c("value" = "value"))
 
 ##look at the number of variables, theres 7 in occurences and 13 in spatial_data. in full_data there's 19 this is because the two datasheets have been added together while the id and sample_id columnms have been merged into one - hence 19
 
@@ -43,25 +66,106 @@ right_data <- right_join(occurences, spatial_data,
 
 ###this returns the same number of observation as full_data, meaning that only spatial_data has ids which are not found in occurences. 
 
-rm(list=ls())
 
 
 
-library(tidyr)
-library(dplyr)
-occurences <- occurrences_2015to2023
-spatial_data <- sampleinfowithlatlong_2015to2023
 
-
-#tidry separate function
 #this function seperates out characters in a column 
 #in the dataset we want to do this to two different colums seperating by different conditions each time
 
-left_data <- left_join(occurences, spatial_data,
-                       by = c("sample_id" = "id"))
 
+
+
+
+### Staying with dyplyr we're going to look at arrange and slice functions
+#slice_max to look at top domins (vice versa for slice_min)
+#slice_sample to look at random amount
+#and slice to look at certain rows 
+
+
+
+
+
+#first arrange
+#this allows us to choose the column we want to use to order the dataset. it automatically arranges that column from lowest to highest and then applies this across the whole dataset
+arrange <- left_data %>% 
+  arrange(LATITUDE)
+
+#look at the data we now have the smallest latitudes at the top, and increasing as we go down the dataset -  a quick check look across to the country column, all the top part now says channel islands !
+#now what about about getting the highest latitudes at the top of the dataset
+
+arrange_desc <- tidy_data %>% 
+  arrange(desc(LATITUDE))
+
+
+#we see a latitiude of 59.9 which is the latitidue of the shetland islands!
+#now that we have arranged our data we can use slice to create new datasheet of the highest or lowest latitiudes
+#first slice - this allows you to pick individual rows by their numerical value, lets pick the middle one
+
+231171/2
+
+slice_data <- tidy_data %>% 
+  arrange(desc(LATITUDE)) %>% 
+  slice(115586)
+
+
+#this gives us the row of a plant species found at most northernly point surveyed - not that useful 
+
+slice_data <- tidy_data %>% 
+  arrange(desc(LATITUDE)) %>% 
+  slice(1:115586)
+
+#this gives us datasheet of 1000 most northerly plants sampled - however what if I said there was an easier way - removing the need for arrange!
+
+#introducing slice_max and slice_min
+
+slice_max <- tidy_data %>% 
+  slice_max(order_by = LATITUDE, n = 115586)
+
+
+#also you may notice that slice_max has returned 1049 values, this is because if the latitidues after our 1000 are also the same, it will return these as well
+#this is very helpful in our case as it doesn't split up quadrats, much simpler and more helpful than that arrange() slice() nonsense
+#slice is still useful for example if you wanted to look specifically at the range around 1000 or to input a sequence looking at every 5th number, or if you didn't need to arrange the data
+
+#now I'll hand it over to you try write the code for what you'd expect for looking at 1000 most southerly species
+
+slice_min <- tidy_data %>% 
+  slice_min(order_by = LATITUDE, n = 115586)
+
+
+#well done, as you can see the same thing happens as with slice_max returning any identical values to so we end up with 1063
+
+#Slicing is not always useful, for example if we were looking at domin it would be simpler to 
+
+
+#what if we want to merge these together to just have top 1000 and bottom 1000 in same 
+
+recombine <- bind_rows
+
+#this is a very useful function but I'm expecting prior knowledge so I'm just going to touch on it briefly
+
+
+
+
+#finally I'll mention slice_sample
+
+#this neat function allows you to take a random sample of the dataset
+
+slice_sample <- tidy_data %>% 
+  slice_sample(n = 1000)
+
+
+
+#now onto some functions in tidyr
+
+
+library(tidyr)
 #THE FUNCTION has three key parts, first which column you want to seperate in our case preferred taxon, then names of the new columns you want to create (genus and species) and finally what you are seperating by. in preferred taxon, genus and species are seperated by a space and so we write " " to indicate this
 #all together it looks like this after using a pipe
+
+#this function seperates out characters in a column 
+#in the dataset we want to do this to two different colums seperating by different conditions each time
+
 seperate_tidyr <- left_data %>% 
   separate(preferred_taxon, into = c("Genus", "Species"), sep = " ")
 
@@ -69,37 +173,84 @@ seperate_tidyr <- left_data %>%
 
 #now instead of preferred_taxon column, we have two new columns Genus and Species
 
-#but what if we want to keep our column keeping them together 
+#but what if we want to keep the original column 
 #simply add the code remove = FALSE
 seperate_tidyr <- left_data %>%
   separate(preferred_taxon, into = c("Genus", "Species"), sep = " ", remove = FALSE)
 
-#another way of writing by spltting by space is "\\s" the two backward slashes here idnicate that whatever is next should be used to seperate and s stands in for a space, these can be chained together if your splitting by multiple characters
-#now the eagle eyed among you might have spotted that we had genuses such as salix which you may know hybridise like crazy and so its hard to determine species and so they have just been written down as genus
-#when we seperate by " " this returns NA in the new species column, what to do about this?
+#another way of writing by splitting by space is "\\s" the two backward slashes here indicate that whatever is next should be used to separate and s stands in for a space, these can be chained together if your splitting by multiple characters
+#now the eagle eyed among you might have spotted that we had genus' such as salix (willow by its common name) which you may know hybridise like crazy and so its hard to determine species and so they have just been recorded by genus only
+#when we separate by " " this returns NA in the new species column for all salix data and similar cases, what to do about this?
 
-#well luckily tidyr has the answer for this as well but first lets seperate the domin column to only keep the numeric values
-#try separating so we get the number from 1-9 in one column and the percentage in the other
+#well luckily tidyr has the answer for this as well
+#we don't want to filter out all the values with NA for species instead just change NA for something more useful
+#we do this using the replace_na (specifically designed for this problem!!) function in the tidyr package
+seperate_tidyr <- seperate_tidyr %>% 
+  replace_na(list(Species = "sp."))
+#now we have replaced all NA values with sp something more useful
+
+
+
+#next lets seperate the domin column to only keep the numeric values (this solves the syntax issue i mentioned earlier)
+
+#try separating so we get the number from 1-10 in one column and the percentage in the other
 
 
 # we can't simply write ". " and so instead must write "\\.\\s" this indicates that we want to split the column at the period followed by a space
 
 #code
-tidy_domin <- seperate_tidyr %>% 
+tidy_domin <- left_data %>% 
   separate(domin, into = c("domin", "percentage"), sep = "\\.\\s")
 
-#
 
 
-#code all together
 
 
-left_data <- left_join(occurences, spatial_data,
+tidy_data <- left_join(occurences, spatial_data,
                        by = c("sample_id" = "id")) %>% 
-              mutate(Genus_species = preferred_taxon) %>% 
-              separate(preferred_taxon, into = c("Genus", "Species"), sep = " ") %>% 
+              separate(preferred_taxon, into = c("Genus", "Species"), sep = " ", remove = FALSE) %>% 
+              separate(domin, into = c("domin", "percentage"), sep = "\\.\\s") %>%
               replace_na(list(Species = "sp.")) %>% 
-              filter(Domin != "NA")
-              
+              filter(domin != "NA")
 
 
+
+#challenge: I want to wrangle data to create a spatial graph with the 100 most southerly trees in the acer genus and same for the 100 most northerly trees. As well as replacing NA in the domin column with something more useful
+
+
+challenge_data <- left_join(occurences, spatial_data,
+                            by = c("sample_id" = "id")) %>%
+  separate(preferred_taxon, into = c("Genus", "Species"), sep = " ", remove = FALSE) %>%
+  replace_na(list(Species = "sp.")) %>% 
+  replace_na(list(domin = "unknown")) %>% 
+  filter(Genus == "Acer") 
+
+max <- challenge_data %>% 
+  slice_max(order_by = LATITUDE, n = 100)
+
+min <- challenge_data %>% 
+  slice_min(order_by = LATITUDE, n = 100)
+
+acer <- bind_rows(min, max)
+
+library(ggthemes)
+library(ggplot2)
+ 
+#just by eyeballing the data you can see that 100 most northerly are spread across 3 bands of latitude, while southerly only 1
+#now to learn how to create a spatial map - only kidding that's a tutorial for another day
+
+
+
+(map <- ggplot(acer, aes(x = LONGITUDE, y = LATITUDE)) +
+  borders("world", colour = "black", fill = "yellow3", size = 0.3) + # Add base map
+  coord_cartesian(xlim = c(-10, 5), ylim = c(48, 62)) + # UK bounds
+  geom_point(aes(), size = 2, alpha = 0.8) + # Points colored by genus
+  theme_map() + # Optional map theme
+  theme(
+    legend.position = "right", # Adjust legend position
+    plot.title = element_text(size = 15, hjust = 0.5)
+  ) +
+  labs(title = "Spatial Distribution of Acer in the UK"))
+
+
+##or could delete code for how to separate out domin, and then ask for 100 samples where sycamore is dominat and 100 samples where it is rare (but this doesn't make much ecological sense to do)
